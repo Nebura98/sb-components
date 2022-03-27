bbf7f4a8ba96ffc7e5edbedf7dc9677ab76d3214
Nebura <hamklieneduprofesional@gmail.com>
2022-03-26 20:12:47 -0600

    Primer commit

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,40 @@
+import './mylabel.css'
+
+export interface PropsMyLabel {
+    /**
+     * El mensaje por defecto será
+     */
+    label: string;
+    /**
+     * El tamaño por defecto será
+     */
+    size: 'normal' | 'h1' | 'h2'| 'h3'; 
+    /**
+     * allCaps representa si la palabra estara en mayuscula
+     */
+    allCaps?: boolean;
+    /**
+     * Color solo cuenta con 3 colores, por defecto será 
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * fontColor es una propiedad opcional la cual mediante una paleta podemos elegir el color
+     */
+    fontColor?: string;
+}
+
+export const MyLabel = ({ 
+    allCaps =  false,
+    color   = 'primary',
+    label   = 'No Label', 
+    size    = 'normal', 
+    fontColor
+}: PropsMyLabel ) => {
+  return (
+    <span 
+        className={ `label ${ size } text-${ color }` } style={{color:`${ fontColor }`}}
+    >
+        { allCaps ? label.toUpperCase() : label }
+    </span>
+  )
+}
