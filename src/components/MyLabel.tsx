8aeac057a71a0b15e15b9db2ada0b0bfeabbc1ff
Nebura <hamklieneduprofesional@gmail.com>
2022-04-02 20:51:17 -0600

    feat: package.json main actualizado

--- src/components/MyLabel.tsx
+++ src/components/MyLabel.tsx
@@ -1,43 +1,45 @@
 import './mylabel.css'
 
 export interface PropsMyLabel {
-    /**
-     * El mensaje por defecto será
-     */
-    label: string;
-    /**
-     * El tamaño por defecto será
-     */
-    size: 'normal' | 'h1' | 'h2'| 'h3'; 
-    /**
-     * allCaps representa si la palabra estara en mayuscula
-     */
-    allCaps?: boolean;
-    /**
-     * Color solo cuenta con 3 colores, por defecto será 
-     */
-    color?: 'primary' | 'secondary' | 'tertiary';
-    /**
-     * fontColor es una propiedad opcional la cual mediante una paleta podemos elegir el color
-     */
-    fontColor?: string;
+  /**
+   * El mensaje por defecto será
+   */
+  label: string;
+  /**
+   * El tamaño por defecto será
+   */
+  size: 'normal' | 'h1' | 'h2' | 'h3';
+  /**
+   * allCaps representa si la palabra estara en mayuscula
+   */
+  allCaps?: boolean;
+  /**
+   * Color solo cuenta con 3 colores, por defecto será 
+   */
+  color?: 'primary' | 'secondary' | 'tertiary';
+  /**
+   * fontColor es una propiedad opcional la cual mediante una paleta podemos elegir el color
+   */
+  fontColor?: string;
 
-    backgroundColor?: string
+  backgroundColor?: string
 }
 
-export const MyLabel = ({ 
-    allCaps =  false,
-    color   = 'primary',
-    label   = 'No Label', 
-    size    = 'normal', 
-    fontColor,
-    backgroundColor='transparent'
-}: PropsMyLabel ) => {
+export const MyLabel = ({
+  allCaps = false,
+  color = 'primary',
+  label = 'No Label',
+  size = 'normal',
+  fontColor,
+  backgroundColor = 'transparent'
+}: PropsMyLabel) => {
   return (
-    <span 
-        className={ `label ${ size } text-${ color }` } style={{color:`${ fontColor }`, backgroundColor}}
+    <span
+      className={`label ${size} text-${color}`} style={{ color: `${fontColor}`, backgroundColor }}
     >
-        { allCaps ? label.toUpperCase() : label }
+      {allCaps ? label.toUpperCase() : label}
     </span>
   )
 }
+
+export default MyLabel;
